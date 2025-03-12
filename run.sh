6d2e52076700d86fd302bef0eda68a93934010b9
zhandos256 <r.zhandos256@gmail.com>
2025-03-12 17:25:46 +0500

    refactor

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ./.venv/bin/activate
+
+cd ./src
+
+if ls migrations/versions/*.py 1>/dev/null 2>&1; then
+    echo "Миграции уже существуют, пропускаем создание новой."
+else
+    alembic revision --autogenerate -m "initial"
+    alembic upgrade head
+fi
+
+python main.py
